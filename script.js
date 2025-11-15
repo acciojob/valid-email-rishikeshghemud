783ffcdc2af789116e626960e6a0dec57a39e1ae
Rishikesh <146565427+rishikeshghemud@users.noreply.github.com>
2025-11-16 00:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function validEmail(str) {
   //your JS code here.
+	if (!str) return false; // handle empty string
+	
+    const local = "[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*";
+
+    const domain = "[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*";
+
+    const extension = "(?:\\.[A-Za-z]{2,3})+";
+
+    const regex = new RegExp(`^${local}@${domain}${extension}$`);
+
+    return regex.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email address."); 
 alert(validEmail(str));
